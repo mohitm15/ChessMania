ea7a26050c35d3427ccd7cc3d60ef1afadf44906
Mohit Maroliya <mmaroliya@gmail.com>
2021-06-20 13:03:09 +0530

    Added the URL of different articles with their content

--- my-blog/src/Pages/ArticlePage.js
+++ my-blog/src/Pages/ArticlePage.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import articleContent from './article-content';
 
-const ArticlePage = () => (
-    <>
-        <h1>Hello , Welcome to Article PAge !</h1>
-        
-    </>
-);
+const ArticlePage = ({match}) => {
+
+    const name = match.params.name;
+    const displayArticle = articleContent.find(displayArticle => displayArticle.name === name);
+    
+    //if article does not exist
+    if(!displayArticle) return<h1>Article does not exist !</h1>
+
+    return (
+        <>
+            <h1>{displayArticle.title} </h1>
+
+                {displayArticle.content.map(item=>(<p key={item.name}>{item}</p> ))}
+            
+        </>
+    )
+};
 
 export default ArticlePage;
