f7634237d7ba3871143c4369dc88316c8b2902cc
Mohit Maroliya <mmaroliya@gmail.com>
2021-06-21 17:13:55 +0530

    Added Upvote functionality for each article

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -2,6 +2,26 @@ import express from 'express';
 
 const app = express();
 
-app.get('/hello',(req,res)=>res.send("Hello"));
-app.get('/hello/:name',(req,res)=>res.send(`Hello ${req.params.name}`))
+const articleInfo = {
+    'learn-react': {
+        upvotes: 0,
+    },
+    'learn-node': {
+        upvotes: 0,
+    },
+    'my-thoughts-on-resumes': {
+        upvotes: 0,
+    },
+}
+
+//incrementing the upvote of the articlesname given by ':name' variable in URL
+//each time a post request is made, upvote inc by 1
+
+app.post('/api/articles/:name/upvote',(req,res)=> {
+    const articleName = req.params.name;
+    articleInfo[articleName].upvotes +=1;
+    res.status(200).send(`${articleName} has now ${articleInfo[articleName].upvotes} upvotes.`)
+
+})
+
 app.listen(8000,()=>console.log("Listening to the port 8000"));
