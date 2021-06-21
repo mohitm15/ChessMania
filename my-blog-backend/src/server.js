b44be5461bba4f787cb7c37019f9f094515ff222
Mohit Maroliya <mmaroliya@gmail.com>
2021-06-21 16:50:05 +0530

    Adding Route Parameter in server.js

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/hello',(req,res)=>res.send("Hello Everyone !"));
-
+app.get('/hello',(req,res)=>res.send("Hello"));
+app.get('/hello/:name',(req,res)=>res.send(`Hello ${req.params.name}`))
 app.listen(8000,()=>console.log("Listening to the port 8000"));
