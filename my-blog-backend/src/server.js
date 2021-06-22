43a9341876605ca048b4b2c38f2deb26f1079e79
Mohit Maroliya <mmaroliya@gmail.com>
2021-06-22 11:16:33 +0530

    Adding MongoDB and connecting it with express js

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,25 +1,32 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+
 
 const app = express();
 
 
 app.use(bodyParser.json());
 
-const articleInfo = {
-    'learn-react': {
-        upvotes: 0,
-        comments:[],
-    },
-    'learn-node': {
-        upvotes: 0,
-        comments:[],
-    },
-    'my-thoughts-on-resumes': {
-        upvotes: 0,
-        comments:[],
-    },
-}
+//connecting with local mongo database
+app.get('/api/articles/:name', async (req,res)=> {
+    try {
+        const articleName = req.params.name;
+
+        const client = await MongoClient.connect('mongodb://localhost:27017', {useNewUrlParser: true});
+        const db = client.db('my-blog');
+    
+        const articleInfo = await db.collection('articles').findOne({ name:articleName});
+        res.status(200).json(articleInfo);
+    
+        client.close();
+    }
+    catch(error) {
+        res.status(500).json({ message:'Error connecting to db',error})
+    }
+})
+
+
 
 //incrementing the upvote of the articlesname given by ':name' variable in URL
 //each time a post request is made, upvote inc by 1
