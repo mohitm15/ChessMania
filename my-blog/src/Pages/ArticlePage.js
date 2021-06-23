b0e07d43dae3ba057dbf9103a1d71e78afb96ea0
Mohit Maroliya <mmaroliya@gmail.com>
2021-06-23 12:22:47 +0530

    Creating the upvote section on articles page.

--- my-blog/src/Pages/ArticlePage.js
+++ my-blog/src/Pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState, useEffect } from 'react';
 import articleContent from './article-content';
 import ArticleList from '../components/ArticleList';
 import NotFoundPage from './NotFoundPage';
@@ -9,6 +9,11 @@ const ArticlePage = ({match}) => {
     const name = match.params.name;
     const displayArticle = articleContent.find(displayArticle => displayArticle.name === name);
     
+    const [articleInfo,setArticleInfo] = useState({ upvotes:0, comments:[] });
+
+    useEffect(()=>{
+        setArticleInfo({upvotes:7});
+    },[]);
     //if article does not exist
     if(!displayArticle) return <NotFoundPage/>
 
@@ -19,7 +24,7 @@ const ArticlePage = ({match}) => {
     return (
         <>
             <h1>{displayArticle.title} </h1>
-
+            <p><em>This post has been upvoted {articleInfo.upvotes} times.</em></p>
                 {displayArticle.content.map(item=>(<p key={item.name}>{item}</p> ))}
             <hr/>
             <ArticleList articlesprops={otherArticles} />
