d620e275f157a9816af00e794d428811340a48cb
Mohit Maroliya <mmaroliya@gmail.com>
2021-07-10 20:59:07 +0530

    Adding input functionality for the timezone section

--- my-blog/src/components/Timezone.js
+++ my-blog/src/components/Timezone.js
@@ -1,11 +1,29 @@
 import React, {useState,useEffect} from 'react';
+import { Button } from 'react-bootstrap';
 
 const TimeZoneComponent = () => {
 
     const [data, setData] = useState([]);
+    const [city,setCity] = useState('');
+    const [printCity, setPrintCity] = useState(false);
 
-    const timeZone = () => {
-        fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+    // function getCity(val) {
+    //     setCity(val.target.value);
+    //     console.log(val.target.value);
+    // }
+
+    const clickfn = () =>
+    {
+        setPrintCity(true);
+        if(printCity)
+        {
+            timeZone(city);
+        }
+    }
+
+
+    const timeZone = (city) => {
+        fetch(`http://worldtimeapi.org/api/timezone/Asia/${city}`)
         .then((response)=>response.json())
         .then((json)=>{
             console.log(json);
@@ -13,18 +31,32 @@ const TimeZoneComponent = () => {
         })
     }
 
+
+
     useEffect(()=> {
             timeZone();
     },[])
 
     let mystr = JSON.stringify(data.datetime);
 
+
+
     return(
+        <>
         <div className="timezone">       
-            Date: {mystr} <br/>
-            Time: {mystr} <br/>
+            <div id="timezone-input">
+                <label>Type Asian City Here..
+                    <br/>
+                <input type="text" placeholder=" Type city here.." onChange={(event)=>setCity(event.target.value)}/>
+                    <br/>
+                <Button className="timezone-button" onClick={()=>clickfn()} >Get Current DateTime</Button>
+                </label>
+            </div>
+            Date: {mystr?.slice(1,11)} <br/>
+            Time: {mystr?.slice(12,20)} <br/>
             Timezone: {data.timezone}
         </div>
+        </>
     );
     
 }
