ef34f38467288a778f561567ed1ae4e71bf2c58c
Mohit Maroliya <mmaroliya@gmail.com>
2022-01-23 18:03:25 +0530

    Changing the country flags API-2

--- my-blog/src/components/lichessProfileComponents/titleHead.js
+++ my-blog/src/components/lichessProfileComponents/titleHead.js
@@ -6,50 +6,53 @@ import {
   BarChartFill,
   CircleFill,
 } from "react-bootstrap-icons";
-import ToastStatusComponent from "./toastStatus";
+// import ToastStatusComponent from "./toastStatus";
 
 const TitleHeadComponent = (props) => {
-  //let nowState = props.isOnline;
-
-  function circleColor(isNow) {
-    //console.log("moh = "+ props.isOnline)
-    return <CircleFill size={16} color={isNow ? "green" : "red"} />;
-  }
-
   return (
     <Alert variant={"success"}>
-      <Row>
-        <Col></Col>
-        <Col className="titleOfUser">{props.titleOfUser?.slice(1, -1)}</Col>
-      </Row>
-      <Row>
-        <Col>
-          <img src={props.srcOfFlag} alt="country_flag" className="h-75 w-24" />
-          <ToastStatusComponent
-            bioOfUser={props.bioOfUser}
-            seenAt={props.seenAt}
-          />
-        </Col>
-        <Col className="userName">{props.username?.slice(1, -1)}</Col>
-      </Row>
-      <Row className="followerRow">
-        <Col>
-          <PeopleFill size={26} color="black" />
-          <br />
-          <b>Following:{props.followers}</b>
-        </Col>
-        <Col>
-          <PersonCheckFill size={26} color="black" /> <br />
-          <b>Followers:{props.following}</b>
-        </Col>
-        <Col>
-          <BarChartFill size={26} color="black" />
-          <br />
-          <b>Online: {props.isOnline}</b> {circleColor(props.isOnline)}
-        </Col>
-        {/* if Online:true => <CircleFill color='green' />
-            else if Online:false => <CircleFill color='red' /> */}
-      </Row>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <Row
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            height: "85px",
+            padding: "auto",
+            margin: "auto",
+          }}
+        >
+          <Col className="userName">
+            <div className="d-flex justify-evenly">
+              <span style={{ marginRight: "15%" }}>
+                <img
+                  src={props.srcOfFlag}
+                  alt="country_flag"
+                  style={{ height: "60px", width:"110px" }}
+                />
+              </span>
+              <span>{props.username?.slice(1, -1)}</span>
+            </div>
+          </Col>
+        </Row>
+        <Row className="followerRow">
+          <Col>
+            <PeopleFill size={26} color="black" />
+            <br />
+            <b>Following:{props.followers}</b>
+          </Col>
+          <Col>
+            <PersonCheckFill size={26} color="black" /> <br />
+            <b>Followers:{props.following}</b>
+          </Col>
+          <Col>
+            <BarChartFill size={26} color="black" />
+            <br />
+            <b>Online: {props.isOnline}</b>{" "}
+            <CircleFill size={16} color={props.isOnline ? "green" : "red"} />
+          </Col>
+        </Row>
+      </div>
     </Alert>
   );
 };
